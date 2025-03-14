0adec051f1be132e0aaecf8506dc6c553a521b6b
Manmath Jukale <jukalemanmath@gmail.com>
2025-03-14 12:01:04 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -3,22 +3,14 @@ class Solution:
 
         if not height:
             return 0
-        
-        l , r = 0 , len(height) - 1
-        leftmax , rightmax = height[l] , height[r]
-
         res = 0
-        while l < r:
-            if leftmax < rightmax:
-                l += 1
-                leftmax = max(leftmax , height[l])
-                res += leftmax - height[l]
-                
-            else:
-                r -= 1
-                rightmax = max(rightmax , height[r])
-                res += rightmax - height[r]
-        return res
+        n = len(height)
 
-                
+        for i in range(n):
+            leftmax = rightmax = height[i]
+            for j in range(i):
+                leftmax = max(leftmax, height[j])
+            for j in range(i+1, n):
+                rightmax = max(rightmax , height[j])
+            res += min(leftmax , rightmax) - height[i]
         return res
