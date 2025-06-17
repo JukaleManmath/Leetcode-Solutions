0d879ff6f914a8167112774adc37dba68c0e2f48
Manmath Jukale <jukalemanmath@gmail.com>
2025-06-17 16:29:11 -0400

    Time: 0 ms (100%), Space: 18.1 MB (43.84%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,15 +1,13 @@
 class Solution:
-    
-
     def generateParenthesis(self, n: int) -> List[str]:
-        def recursion(s, open_cts, close_cts, arr , n):
+        def recursion(s, open_cts, close_cts, arr):
             if len(s) == 2 * n and open_cts == close_cts:
                 arr.append(s)
             
             if open_cts < n:
-                recursion(s+ "(", open_cts+1, close_cts, arr, n)
+                recursion(s+ "(", open_cts+1, close_cts, arr)
             if close_cts < open_cts:
-                recursion(s+ ")", open_cts, close_cts+1, arr, n)
+                recursion(s+ ")", open_cts, close_cts+1, arr)
             return arr
-        res = recursion("", 0, 0 , [], n)
+        res = recursion("", 0, 0 , [])
         return res
