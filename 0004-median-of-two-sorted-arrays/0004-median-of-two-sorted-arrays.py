c2c2f206f3affef665f4a5fca86186de59497320
Manmath Jukale <jukalemanmath@gmail.com>
2025-06-24 20:08:49 -0400

    Time: 3 ms (52.31%), Space: 17.9 MB (82.95%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,26 +1,34 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merged_array = []
-        m , n = len(nums1), len(nums2)
-        i , j = 0, 0
-        while i < m and j < n:
-            if nums1[i] <= nums2[j]:
-                merged_array.append(nums1[i])
-                i += 1
-            else:
-                merged_array.append(nums2[j])
-                j += 1
-        while i < m :
-            merged_array.append(nums1[i])
-            i+= 1
-        while j < n:
-            merged_array.append(nums2[j])
-            j += 1
+        n1 , n2 = len(nums1), len(nums2)
+        if n1 > n2:
+            return self.findMedianSortedArrays(nums2,nums1)
         
-        if (m + n) % 2 == 0:
-            median = merged_array[(m+n-1)//2] + merged_array[((m+n-1)//2) + 1]
-            median = median / 2  
-        else:
-            median = merged_array[(m +n-1)//2 ]
+        n = n1 + n2
+        left = (n1 + n2 + 1) // 2
+        low , high = 0 , n1
+        while low <= high:
+            mid1 = (low + high) // 2
+            mid2 = left - mid1
 
-        return median
+            l1 , l2 , r1, r2 = float('-inf'), float('-inf'), float('inf'),float('inf')
+
+            if mid1 < n1:
+                r1 = nums1[mid1]
+            if mid2 < n2:
+                r2 = nums2[mid2]
+            if mid1 -1 >= 0:
+                l1 = nums1[mid1 - 1]
+            if mid2 -1 >= 0:
+                l2 = nums2[mid2 - 1]
+            
+            if l1 <= r2 and l2 <= r1:
+                if n % 2 == 1:
+                    return max(l1, l2)
+                else:
+                    return (float(max(l1, l2)) + float(min(r1, r2))) / 2.0
+            elif l1 > r2:
+                high = mid1 -1
+            else:
+                low = mid1 + 1
+        return 0
