0e7bcdc873581c19904c804e3fbd988e2d084ecf
Manmath Jukale <jukalemanmath@gmail.com>
2025-06-30 23:44:32 -0400

    Time: 22 ms (83.99%), Space: 30.1 MB (32.08%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,13 +1,13 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        # Brute Force
-        nums.sort()
-        for i in range(len(nums) -1):
-            if nums[i] == nums[i+1]:
-                return nums[i]
-        return -1
+        # Brute Force - O(nlogn)
+        # nums.sort()
+        # for i in range(len(nums) -1):
+        #     if nums[i] == nums[i+1]:
+        #         return nums[i]
+        # return -1
 
-        # # map
+        # # map - O(n) and space - O(n)
         # map = {}
         # for i in nums:
         #     map[i] = 1 + map.get(i,0)
@@ -15,10 +15,26 @@ class Solution:
         #         return i
         # return -1
 
-        #set
+        #set - O(n) and space - O(n)
         # seen = set()
         # for num in nums:
         #     if num in seen:
         #         return num
         #     seen.add(num)
         # return -1
+
+        # floyd's slow and fast pointer approach - O(n) and O(1) space
+        slow , fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if fast == slow:
+                break
+        
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                break
+        return slow
