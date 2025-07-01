3d70f52f5ce1c83db159603150511caae019dacd
Manmath Jukale <jukalemanmath@gmail.com>
2025-06-30 20:32:15 -0400

    Time: 83 ms (11.54%), Space: 30 MB (70.85%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,5 +1,12 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        # Brute Force
+        nums.sort()
+        for i in range(len(nums) -1):
+            if nums[i] == nums[i+1]:
+                return nums[i]
+        return -1
+
         # # map
         # map = {}
         # for i in nums:
@@ -9,9 +16,9 @@ class Solution:
         # return -1
 
         #set
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return num
-            seen.add(num)
-        return -1
+        # seen = set()
+        # for num in nums:
+        #     if num in seen:
+        #         return num
+        #     seen.add(num)
+        # return -1
