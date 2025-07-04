b163406a6761e7e677468fd3da33a5df489cdc5d
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-04 17:48:34 -0400

    Time: 0 ms (100%), Space: 18.7 MB (18.27%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,23 +5,52 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        arr = []
-        while head:
-            arr.append(head)
-            head = head.next
+        # O(n) -time but space also - O(n)
+        # arr = []
+        # while head:
+        #     arr.append(head)
+        #     head = head.next
         
-        n = len(arr)
-        i = 0
-        while i+k <= len(arr):
-            left = i
-            right = i + k - 1
-            while left <= right:
-                arr[left], arr[right] = arr[right], arr[left]
-                left+= 1
-                right -= 1
-            i += k
+        # n = len(arr)
+        # i = 0
+        # while i+k <= len(arr):
+        #     left = i
+        #     right = i + k - 1
+        #     while left <= right:
+        #         arr[left], arr[right] = arr[right], arr[left]
+        #         left+= 1
+        #         right -= 1
+        #     i += k
             
-        for l in range(len(arr)-1):
-            arr[l].next = arr[l+1]
-        arr[n-1].next = None
-        return arr[0]
+        # for l in range(len(arr)-1):
+        #     arr[l].next = arr[l+1]
+        # arr[n-1].next = None
+        # return arr[0]
+
+        dummy = ListNode(0, head)
+        groupPrev = dummy 
+
+        while True:
+            kth = self.getKth(groupPrev, k)
+            if kth is None:
+                break
+            groupNext = kth.next
+
+            prev, curr = kth.next, groupPrev.next
+            while curr != groupNext:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr =temp
+            
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+
+        return dummy.next
+            
+    def getKth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr
