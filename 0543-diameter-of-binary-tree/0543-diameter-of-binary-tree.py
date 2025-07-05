0e19281352bbf50212edc6770d3b7f8102a4783c
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-05 16:20:53 -0400

    Time: 3 ms (94.37%), Space: 20.9 MB (49.4%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -5,15 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxHeight(self, root):
-        if root is None:
-            return 0
-        return 1 + max(self.maxHeight(root.left), self.maxHeight(root.right))
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
-        
-        diameter = self.maxHeight(root.left) + self.maxHeight(root.right)
 
-        sub = max(self.diameterOfBinaryTree(root.left), self.diameterOfBinaryTree(root.right))
-        return max(diameter, sub)
+    # Brute force - O(n**2)
+    # def maxHeight(self, root):
+    #     if root is None:
+    #         return 0
+    #     return 1 + max(self.maxHeight(root.left), self.maxHeight(root.right))
+    # def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+    #     if root is None:
+    #         return 0
+        
+    #     diameter = self.maxHeight(root.left) + self.maxHeight(root.right)
+
+    #     sub = max(self.diameterOfBinaryTree(root.left), self.diameterOfBinaryTree(root.right))
+    #     return max(diameter, sub)
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        def dfs(root):
+            nonlocal res
+            if root is None:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            res = max(res, left+right)
+            return 1 + max(left, right)
+        dfs(root)
+        return res
+
