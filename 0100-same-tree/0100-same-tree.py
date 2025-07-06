00c7913c9b074350a8337829fb93b5d59d4f70b3
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-06 12:26:06 -0400

    Time: 0 ms (100%), Space: 17.7 MB (65.2%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,19 +6,26 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        def dfs(root, res):
-            if root is None:
-                res.append(None)
-                return 
-            res.append(root.val)
-            dfs(root.left, res)
-            dfs(root.right, res)
-        res1 = []
-        res2 = []
-        dfs(p, res1)
-        dfs(q, res2)
+        # extra space
+        # def dfs(root, res):
+        #     if root is None:
+        #         res.append(None)
+        #         return 
+        #     res.append(root.val)
+        #     dfs(root.left, res)
+        #     dfs(root.right, res)
+        # res1 = []
+        # res2 = []
+        # dfs(p, res1)
+        # dfs(q, res2)
         
-        return res1 == res2
+        # return res1 == res2
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
         
         
 
