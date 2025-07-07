17dcec76d45d47026da3f3566711abd727e4fee6
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-07 16:37:42 -0400

    Time: 165 ms (10.52%), Space: 32.3 MB (33.85%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -6,19 +6,32 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        max_till = root.val
-        good = 0
-        q = deque()
-        q.append((root, max_till))
-        while q:
-            node, max_till = q.popleft()
-            if node.val >= max_till:
-                good += 1
-            if node.left:
-                q.append((node.left, max(max_till, node.val)))
-            if node.right:
-                q.append((node.right, max(max_till, node.val)))
-        return good
+        #  BFS
+        # max_till = root.val
+        # good = 0
+        # q = deque()
+        # q.append((root, max_till))
+        # while q:
+        #     node, max_till = q.popleft()
+        #     if node.val >= max_till:
+        #         good += 1
+        #     if node.left:
+        #         q.append((node.left, max(max_till, node.val)))
+        #     if node.right:
+        #         q.append((node.right, max(max_till, node.val)))
+        # return good
+
+        # DFS
+        def dfs(root, max_till):
+            if root is None:
+                return 0
+            res = 1 if root.val >= max_till else 0
+            res += dfs(root.left , max(max_till, root.val))
+            res += dfs(root.right , max(max_till, root.val))
+            return res
+        return dfs(root, root.val)
+            
+
 
                 
 
