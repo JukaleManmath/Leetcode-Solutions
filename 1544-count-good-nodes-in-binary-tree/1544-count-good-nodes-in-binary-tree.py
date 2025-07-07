304cc9371aa2abd296b5be1a8c4615941ba366a4
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-07 16:32:48 -0400

    Time: 159 ms (17.64%), Space: 32.1 MB (82.14%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -12,16 +12,12 @@ class Solution:
         q.append((root, max_till))
         while q:
             node, max_till = q.popleft()
-            if node:
-                if node.val >= max_till:
-                    good += 1
-                if node.val > max_till:
-                    max_till = node.val
-                
-                if node.left:
-                    q.append((node.left, max_till))
-                if node.right:
-                    q.append((node.right, max_till))
+            if node.val >= max_till:
+                good += 1
+            if node.left:
+                q.append((node.left, max(max_till, node.val)))
+            if node.right:
+                q.append((node.right, max(max_till, node.val)))
         return good
 
                 
