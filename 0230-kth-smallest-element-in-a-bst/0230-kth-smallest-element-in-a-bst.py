253f3ffcd76f9f9a5687bf444dba1495ed447edd
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-08 17:21:09 -0400

    Time: 0 ms (100%), Space: 21 MB (93.3%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,18 +6,30 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-    
-        def dfs(root, res):
-            if not root:
-                return res
-            res.append(root.val)
+        # DFS- naive approach O(nlogn) - due to sorting
+        # def dfs(root, res):
+        #     if not root:
+        #         return res
+        #     res.append(root.val)
 
-            if root.left:
-                res = dfs(root.left, res)
-            if root.right:
-                res = dfs(root.right, res)
+        #     if root.left:
+        #         res = dfs(root.left, res)
+        #     if root.right:
+        #         res = dfs(root.right, res)
             
-            return res
-        res = dfs(root, [])
-        res.sort()
-        return res[k-1]
+        #     return res
+        # res = dfs(root, [])
+        # res.sort()
+        # return res[k-1]
+
+        # using in inorder we can skip the sorting part left -> root -> right it is already a sorted sequence
+        arr = []
+        def dfs(root):
+            if not root:
+                return
+            
+            dfs(root.left)
+            arr.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        return arr[k-1]
