c158fad70832baa812df53515a79e2717c08b5eb
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-10 09:56:28 -0400

    Time: 22 ms (33.47%), Space: 23.7 MB (60.03%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -1,19 +1,28 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.heap = []
-        self.k = k
-        for i in nums:
-            heapq.heappush(self.heap, i)
-            if len(self.heap) > self.k:
-                heapq.heappop(self.heap)
+        # self.heap = []
+        # self.k = k
+        # for i in nums:
+        #     heapq.heappush(self.heap, i)
+        #     if len(self.heap) > self.k:
+        #         heapq.heappop(self.heap)
+
+        self.min_heap, self.k = nums, k
+        heapq.heapify(self.min_heap)
+        while len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.heap, val)
-        if len(self.heap) > self.k:
-            heapq.heappop(self.heap)
-        return self.heap[0]
+        # heapq.heappush(self.heap, val)
+        # if len(self.heap) > self.k:
+        #     heapq.heappop(self.heap)
+        # return self.heap[0]
         
+        heapq.heappush(self.min_heap,val)
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+        return self.min_heap[0]
 
 
 # Your KthLargest object will be instantiated and called as such:
