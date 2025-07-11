3632fd18d320084d5526d9a5652ab3192b2778bf
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-10 23:53:31 -0400

    Time: 90 ms (24.89%), Space: 23.1 MB (21.32%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -0,0 +1,14 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap = []
+        res = []
+        for i in points:
+            x , y = i[0], i[1]
+            dis = abs(x**2 + y**2)
+            heapq.heappush(minHeap, [dis, x, y])
+
+        while k > 0:
+            dis, x, y = heapq.heappop(minHeap)
+            res.append([x, y])
+            k -= 1
+        return res
