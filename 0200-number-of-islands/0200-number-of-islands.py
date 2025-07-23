3307cf25141989a92d221e038e90c34d8ca058b2
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-22 22:54:41 -0400

    Time: 240 ms (70.3%), Space: 20.2 MB (57.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -23,21 +23,48 @@ class Solution:
         #             count += 1
         # return count
 
-        #dfs -2 
+        #dfs -2 recursive approach
+        # row , col = len(grid), len(grid[0])
+        # isIlands = 0
+        # directions = [[1,0], [-1,0], [0,1], [0, -1]]
+
+        # def dfs(r , c):
+        #     if r < 0 or c <0 or r >= row or c>= col or grid[r][c] == "0":
+        #         return
+            
+        #     grid[r][c] = "0"
+        #     for dr, dc in directions:
+        #         dfs(r + dr, c + dc)
+        # for r in range(row):
+        #     for c in range(col):
+        #         if grid[r][c] == "1":
+        #             dfs(r, c)
+        #             isIlands += 1
+        # return isIlands
+
+        # bfs - Iterative approach
         row , col = len(grid), len(grid[0])
         isIlands = 0
         directions = [[1,0], [-1,0], [0,1], [0, -1]]
 
-        def dfs(r , c):
-            if r < 0 or c <0 or r >= row or c>= col or grid[r][c] == "0":
-                return
-            
+        def bfs(r, c):
+            q = deque()
             grid[r][c] = "0"
-            for dr, dc in directions:
-                dfs(r + dr, c + dc)
+            q.append((r, c))
+
+            while q:
+                rr, rc = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = rr+dr , rc + dc
+                    if nr < 0 or nc < 0 or nr >= row or nc >= col or grid[nr][nc] == "0":
+                        continue
+                    q.append((nr, nc))
+                    grid[nr][nc] = "0" 
+
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == "1":
-                    dfs(r, c)
+                    bfs(r, c)
                     isIlands += 1
         return isIlands
+
