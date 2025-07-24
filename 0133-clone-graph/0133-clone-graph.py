291ffabeff0de1d0a4b983f43630061c99669859
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-24 11:00:34 -0400

    Time: 51 ms (10.11%), Space: 18.1 MB (82.82%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -9,15 +9,31 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
         oldToNew = {}
 
-        def dfs(node):
-            if node in oldToNew:
-                return oldToNew[node]
+        # def dfs(node):
+        #     if node in oldToNew:
+        #         return oldToNew[node]
             
-            cp = Node(node.val)
-            oldToNew[node] = cp
-            for i in node.neighbors:
-                cp.neighbors.append(dfs(i))
-            return cp
-        return dfs(node) if node else None
+        #     cp = Node(node.val)
+        #     oldToNew[node] = cp
+        #     for i in node.neighbors:
+        #         cp.neighbors.append(dfs(i))
+        #     return cp
+        # return dfs(node) if node else None
+
+        oldToNew[node] = Node(node.val)
+        q = deque([node])
+
+        while q:
+            curr = q.popleft()
+            for nei in curr.neighbors:
+                if nei not in oldToNew:
+                    oldToNew[nei] = Node(nei.val)
+                    q.append(nei)
+                oldToNew[curr].neighbors.append(oldToNew[nei])
+        return oldToNew[node]
+
+
