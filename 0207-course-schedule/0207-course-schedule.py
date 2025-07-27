c35df6aabd280d729963abcc7aa2c6022db6c447
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-27 12:23:35 -0400

    Time: 0 ms (100%), Space: 19.2 MB (63.01%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,28 @@
+class Solution:
+    def constructadj(self, n , prerequisites):
+        adj = [[] for _ in range(n)]
+        for ai, bi in prerequisites:
+            adj[bi].append(ai)
+        return adj
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = self.constructadj(numCourses, prerequisites)
+        indegree = [0] * numCourses
+
+        for bi in range(numCourses):
+            for ai in adj[bi]:
+                indegree[ai] += 1
+        
+        q = deque([i for i in range(numCourses) if indegree[i] == 0])
+
+        res = []
+        while q:
+            node = q.popleft()
+            res.append(node)
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        
+        if len(res) != numCourses:
+            return False
+        return True
