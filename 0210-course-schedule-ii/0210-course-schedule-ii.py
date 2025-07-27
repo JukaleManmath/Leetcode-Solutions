c64fb140b3776ef8959250409e74bb280095c149
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-27 12:42:42 -0400

    Time: 7 ms (26.64%), Space: 18.9 MB (95.6%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = [[] for _ in range(numCourses)]
+        for ai, bi in prerequisites:
+            adj[bi].append(ai)
+        
+        indegree = [0] * numCourses
+
+        for bi in range(numCourses):
+            for ai in adj[bi]:
+                indegree[ai] += 1
+        
+        q = deque([i for i in range(numCourses) if indegree[i] == 0])
+        res = []
+        while q:
+            node = q.popleft()
+            res.append(node)
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        return res if len(res) == numCourses else []
