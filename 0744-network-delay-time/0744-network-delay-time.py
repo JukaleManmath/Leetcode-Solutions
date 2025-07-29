a7f2b35b2b1cb9dc6e8527ef5be120a286d48b5f
Manmath Jukale <jukalemanmath@gmail.com>
2025-07-28 23:04:07 -0400

    Time: 393 ms (31.22%), Space: 19.5 MB (92.63%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -3,16 +3,32 @@ class Solution:
         edges = defaultdict(list)
         for u, v, w in times:
             edges[u].append((v,w))
-        dist = {node: float("inf") for node in range(1, n+1)}
 
-        def dfs(node, time):
-            if time >= dist[node]:
-                return
-            dist[node] = time
-            for nei , t in edges[node]:
-                dfs(nei, t + time)
-        dfs(k,0)
-        res = max(dist.values())
-        return res if res < float("inf") else -1
+        # brute force - O(V*E)
+        # dist = {node: float("inf") for node in range(1, n+1)}
+
+        # def dfs(node, time):
+        #     if time >= dist[node]:
+        #         return
+        #     dist[node] = time
+        #     for nei , t in edges[node]:
+        #         dfs(nei, t + time)
+        # dfs(k,0)
+        # res = max(dist.values())
+        # return res if res < float("inf") else -1
             
-        
+        # Dijkstra's algoithm - Optimal O(ElogV) - uses minHeap for non-negative wts and selects the shortest next possible node having wt
+        minheap = [(0, k)]
+        t = 0
+        visited = set()
+
+        while minheap:
+            t1, n1 = heapq.heappop(minheap)
+            if n1 in visited:
+                continue
+            visited.add(n1)
+            t = t1
+            for n2, t2 in edges[n1]:
+                if n2 not in visited:
+                    heapq.heappush(minheap, (t1 + t2, n2))
+        return t if len(visited) == n else -1
