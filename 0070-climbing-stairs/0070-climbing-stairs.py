c47c66f67e836771c8bc4c6b1578599e2c6724b8
Manmath Jukale <jukalemanmath@gmail.com>
2025-08-04 13:37:31 -0400

    Time: 0 ms (100%), Space: 17.9 MB (29.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        #  Memoization added to recursion - Top-down approach
+        #  Memoization added to recursion - Top-down approach O(n) - space and time
         # steps= [-1] * (n+1)
         # def dfs(i):
         #     if i == 0 or i == 1:
@@ -12,11 +12,19 @@ class Solution:
         # return dfs(n)
         
 
-        #  Bottom-up approach
-        if n <= 2:
-            return n
-        dp = [0] * (n+1)
-        dp[1] , dp[2] = 1, 2
-        for i in range(3, n + 1):
-            dp[i] = dp[i- 1] + dp[i-2]
-        return dp[n]
+        #  Bottom-up approach O(n) - space and time
+        # if n <= 2:
+        #     return n
+        # dp = [0] * (n+1)
+        # dp[1] , dp[2] = 1, 2
+        # for i in range(3, n + 1):
+        #     dp[i] = dp[i- 1] + dp[i-2]
+        # return dp[n]
+
+        # Space optimized dp
+        one, two = 1, 1  #(0 and 1 steps are assigned val)
+        for i in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
