e202c5091523d34072b350eeca53a4f5b2da2525
Manmath Jukale <jukalemanmath@gmail.com>
2025-08-04 20:30:13 -0400

    Time: 23 ms (49.18%), Space: 18 MB (17.4%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.py
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.py
@@ -2,16 +2,14 @@ class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
         placed = [False] * len(fruits)
         n = len(fruits)
-        l , r = 0 , 0
+        cnt = 0
         for i in range(n):
             for j in range(n):
                 if fruits[i] <= baskets[j] and not placed[j]:
-                    placed[j] = True
+                    baskets[j] = -1
                     break
-        
-        cnt = 0
-        for c in placed:
-            if c == False:
+            else:
                 cnt += 1
+        
         return cnt
 
