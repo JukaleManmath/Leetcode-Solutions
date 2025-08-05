5bd195b71b20dca6d6d284e164cf335f8b590f4d
Manmath Jukale <jukalemanmath@gmail.com>
2025-08-05 18:19:23 -0400

    Time: 0 ms (100%), Space: 17.7 MB (82.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,27 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        #  Memoization O(n) - Top down
+        # n = len(nums)
+        # memo =  [-1] * (n+1)
+        # def dfs(i):
+        #     if i >= n:
+        #         return 0
+        #     if memo[i] != -1:
+        #         return memo[i]
+        #     memo[i] =  max(nums[i] + dfs(i +2) , dfs(i + 1))
+        #     return memo[i]
+        # return dfs(0)
+
+        # DP  - Bottom Up
         n = len(nums)
-        memo =  [-1] * (n+1)
-        def dfs(i):
-            if i >= n:
-                return 0
-            if memo[i] != -1:
-                return memo[i]
-            memo[i] =  max(nums[i] + dfs(i +2) , dfs(i + 1))
-            return memo[i]
-        return dfs(0)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+    
+        dp = [0] * (n+1)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        return dp[n-1]
