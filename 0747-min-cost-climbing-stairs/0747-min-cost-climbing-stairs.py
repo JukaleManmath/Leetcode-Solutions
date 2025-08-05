3c79b89d57699b96469ba1011251b1f91ae883ae
Manmath Jukale <jukalemanmath@gmail.com>
2025-08-05 00:36:33 -0400

    Time: 0 ms (100%), Space: 17.9 MB (46.13%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,16 +1,23 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        cache = [-1] * (n + 1)
+        # Top - Down Memoization
+        # n = len(cost)
+        # cache = [-1] * (n + 1)
 
-        def dfs(i):
-            if i >= n:
-                return 0
+        # def dfs(i):
+        #     if i >= n:
+        #         return 0
             
-            if cache[i] != -1:
-                return cache[i]
+        #     if cache[i] != -1:
+        #         return cache[i]
             
-            cache[i] = cost[i] + min(dfs(i + 1), dfs(i +2))
-            return cache[i]
+        #     cache[i] = cost[i] + min(dfs(i + 1), dfs(i +2))
+        #     return cache[i]
     
-        return min(dfs(0), dfs(1))
+        # return min(dfs(0), dfs(1))
+
+        n = len(cost)
+        dp = [0] * (n + 1)
+        for i in range(2, n+1):
+            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i - 2])
+        return dp[n]
