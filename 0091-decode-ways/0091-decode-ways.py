80ec6448161f6ac79414b0dcd18a4101d99577bb
Manmath Jukale <jukalemanmath@gmail.com>
2025-08-11 12:57:04 -0400

    Time: 0 ms (100%), Space: 17.7 MB (68.84%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,17 +1,30 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        memo = [-1] * (len(s) + 1)
-        def dfs(i):
-            if i == len(s):
-                return 1
-            if memo[i] != -1:
-                return memo[i]
-            if s[i] == '0':
-                return 0
+        # memo = [-1] * (len(s) + 1)
+        # def dfs(i):
+        #     if i == len(s):
+        #         return 1
+        #     if memo[i] != -1:
+        #         return memo[i]
+        #     if s[i] == '0':
+        #         return 0
 
-            memo[i] = dfs(i+1)
-            if i + 1 <= len(s) -1:
-                if(s[i] == "1" or s[i]=="2" and s[i +1] <"7"):
-                    memo[i] += dfs(i+2)
-            return memo[i]
-        return dfs(0)
+        #     memo[i] = dfs(i+1)
+        #     if i + 1 <= len(s) -1:
+        #         if(s[i] == "1" or s[i]=="2" and s[i +1] <"7"):
+        #             memo[i] += dfs(i+2)
+        #     return memo[i]
+        # return dfs(0)
+
+        # Bottom up approach - dp
+        dp = {len(s): 1}
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == '0':
+                dp[i] = 0
+            else:
+                dp[i] = dp[i+1]
+            
+            if i + 1 <len(s) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456"):
+                dp[i] += dp[i+2]
+
+        return dp[0] 
