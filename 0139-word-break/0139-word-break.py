e65932d5ffb867b43e0ee09d968ec667d5bce1ad
Manmath Jukale <jukalemanmath@gmail.com>
2025-08-12 20:46:50 -0400

    Time: 6 ms (27.94%), Space: 18.2 MB (12.89%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        words = set(wordDict)
+        memo = {len(s) : True}
+        n = len(s)
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+        
+            for j in range(i + 1, n+1):
+                if s[i:j] in words and dfs(j):
+                    memo[i] = True
+                    return True
+            memo[i] = False
+            return False
+        return dfs(0)
