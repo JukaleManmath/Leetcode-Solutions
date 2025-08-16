ac8a4b07e92182d64b991ba5e1b9727c9b17d157
Manmath Jukale <jukalemanmath@gmail.com>
2025-08-16 12:34:51 -0400

    Time: 1174 ms (15.47%), Space: 35.9 MB (79.35%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        memo = [[-1] * (len(text2)) for _ in range(len(text1))]
+        def dfs(s1, s2):
+            if not s1 or not s2:
+                return 0
+            i , j = len(s1) - 1, len(s2) -1
+            if memo[i][j] != -1:
+                return memo[i][j]
+            if s1[-1] == s2[-1]:
+                memo[i][j] =  1 + dfs(s1[:-1], s2[:-1])
+                return memo[i][j]
+            memo[i][j] =  max(dfs(s1[:-1], s2), dfs(s1, s2[:-1]))
+            return memo[i][j]
+
+        return dfs(text1, text2)
