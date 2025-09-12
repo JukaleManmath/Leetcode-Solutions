7c78d45da38b0d83e6d5c3f74aa58f98a5acdfd9
Manmath Jukale <jukalemanmath@gmail.com>
2025-09-12 01:54:33 -0400

    Time: 11 ms (90.59%), Space: 18.8 MB (27.52%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,9 +1,21 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        canjump = 0
+        #we ccan keep track of the maximum index we can reach at current point and
+        # if we can not move forward return False 
+        # canjump = 0
+        # n = len(nums)
+        # for i in range(n-1):
+        #     if canjump < i:
+        #         return False
+        #     canjump = max(canjump,i + nums[i])
+        # return canjump >= n -1
+
+        # we can also check in reverse that we can reach to the last index by coming back to 
+        # first index and changing the target like wise
+
         n = len(nums)
-        for i in range(n-1):
-            if canjump < i:
-                return False
-            canjump = max(canjump,i + nums[i])
-        return canjump >= n -1
+        target = n -1
+        for i in range(n-2,-1,-1):
+            if i + nums[i] >= target:
+                target = i
+        return target == 0
