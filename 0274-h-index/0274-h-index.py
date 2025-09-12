7bca1431fef5a008ab38cfc8a523f3cb2b0b3a2a
Manmath Jukale <jukalemanmath@gmail.com>
2025-09-12 13:11:49 -0400

    Time: 0 ms (100%), Space: 18.1 MB (62.84%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,13 +1,33 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        max_h = 0
-        n = len(citations)
-        for i in range(1, n+1):
-            cnt = 0
-            for j in range(n):
-                if citations[j] >= i:
-                    cnt += 1
-                if cnt == i:
-                    max_h = max(max_h, cnt)
-                    break
-        return max_h
+
+        #  Brute force - O(n2)
+        # max_h = 0
+        # n = len(citations)
+        # for i in range(1, n+1):
+        #     cnt = 0
+        #     for j in range(n):
+        #         if citations[j] >= i:
+        #             cnt += 1
+        #         if cnt == i:
+        #             max_h = max(max_h, cnt)
+        #             break
+        # return max_h
+
+        # so the optimized wayy is to like sort or store the count of papers , index as the number of citations and 
+        # we know the citations are between 0 - n
+        # and we come from max h(last index) in reverse order checing if the h has atleast h paper
+        # we use commulatative for it
+
+        paper = len(citations)
+        cnt = [0] * (paper + 1)
+
+        for citation in citations:
+            cnt[min(citation, paper)] += 1
+        
+        comm = 0
+        for i in range(paper, -1, -1):
+            comm += cnt[i]
+            if comm >= i:
+                return i
+                
