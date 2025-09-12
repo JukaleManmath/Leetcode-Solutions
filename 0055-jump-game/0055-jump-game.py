0edb9c6617434113d989c8467be5580f08e0f090
Manmath Jukale <jukalemanmath@gmail.com>
2025-09-12 01:50:12 -0400

    Time: 34 ms (47.62%), Space: 18.5 MB (68.45%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -3,7 +3,7 @@ class Solution:
         canjump = 0
         n = len(nums)
         for i in range(n-1):
-            if canjump == i and nums[i] == 0:
+            if canjump < i:
                 return False
             canjump = max(canjump,i + nums[i])
         return canjump >= n -1
