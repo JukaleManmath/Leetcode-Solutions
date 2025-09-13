633b8c1f0f31d3ff9abce7c0dac16a92243f206d
Manmath Jukale <jukalemanmath@gmail.com>
2025-09-13 19:53:01 -0400

    Time: 33 ms (33.51%), Space: 23.3 MB (83.15%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,49 +1,11 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        # n = len(gas)
-        # flag = True
-        # res = -1
-        # j = 0
-        # while j < n:
-        #     total = 0
-        #     for i in range(0, n):
-        #         k = (i+j)% n
-        #         total += gas[k] - cost[k]
-        #         if total < 0:
-        #             flag = False
-        #             break
-        #     if flag:
-        #         res = j
-        #         break
-        #     else:
-        #         flag = True
-        #         j += 1
-        # return res
-
-        # if sum(gas) < sum(cost):
-        #     return -1
-        # n = len(gas)
-        # res = 0
-        # total = 0
-        # for i in range(n):
-        #     total += gas[i] - cost[i]
-        #     if total < 0:
-        #         total = 0
-        #         res = i +1
-                
-        # return res 
-
-        n = len(gas)
-        r , l = n -1, 0
-        tank = gas[r] - cost[r]
-        while r > l:
-            if tank < 0:
-                r -= 1
-                tank += gas[r] - cost[r]
-            else:
-                tank += gas[l] - cost[l]
-                l += 1
-        return r if tank >= 0 else -1
-
-
-
+        total , curr, start = 0, 0, 0
+        for i in range(len(gas)):
+            curr += gas[i] - cost[i]
+            total += gas[i] - cost[i]
+            if curr < 0:
+                start = i + 1
+                curr = 0
+            
+        return start if total >= 0 else -1
