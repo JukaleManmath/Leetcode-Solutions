7e97116849d44b72d7420258c83f7621fbee12d3
Manmath Jukale <jukalemanmath@gmail.com>
2025-09-14 15:59:31 -0400

    Time: 8 ms (78.07%), Space: 19.2 MB (69.77%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,18 +1,33 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
-        left = [0] * n
-        right = [0] * n
+        # left = [0] * n
+        # right = [0] * n
 
-        left[0] = height[0]
-        for i in range(1, n):
-            left[i] = max(left[i-1],height[i] )
+        # left[0] = height[0]
+        # for i in range(1, n):
+        #     left[i] = max(left[i-1],height[i] )
         
-        right[n-1] = height[n-1]
-        for i in range(n-2, -1, -1):
-            right[i] = max(right[i+1],height[i] )
+        # right[n-1] = height[n-1]
+        # for i in range(n-2, -1, -1):
+        #     right[i] = max(right[i+1],height[i] )
         
+        # res = 0
+        # for i in range(n):
+        #     res += min(left[i],right[i]) - height[i]
+        # return res
+
+        l , r = 0 , n -1
+        left, right = height[l] , height[r]
         res = 0
-        for i in range(n):
-            res += min(left[i],right[i]) - height[i]
+        while l < r:
+            if left <= right:
+                l += 1
+                left = max(left, height[l])
+                res += left - height[l]
+            else:
+                r -= 1
+                right = max(right, height[r])
+                res += right - height[r]
+        
         return res
