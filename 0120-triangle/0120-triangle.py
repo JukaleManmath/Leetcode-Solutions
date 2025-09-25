37090e0ee48e00d2d31f498f0a0dd91ae03bc2f6
Manmath Jukale <jukalemanmath@gmail.com>
2025-09-24 23:02:17 -0400

    Time: 8 ms (22.03%), Space: 19 MB (62.18%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,16 +1,24 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        n = len(triangle)
-        path = {}
+        #  Recursive approach
+        # n = len(triangle)
+        # path = {}
 
-        def dfs(i , level):
-            if level == n -1:
-                return triangle[level][i]
-            if (i,level) in path:
-                return path[(i,level)]
+        # def dfs(i , level):
+        #     if level == n -1:
+        #         return triangle[level][i]
+        #     if (i,level) in path:
+        #         return path[(i,level)]
             
-            path[(i,level)]= min(triangle[level][i] + dfs(i , level + 1),
-                        triangle[level][i] + dfs(i + 1 , level + 1))
-            return path[(i,level)]
+        #     path[(i,level)]= min(triangle[level][i] + dfs(i , level + 1),
+        #                 triangle[level][i] + dfs(i + 1 , level + 1))
+        #     return path[(i,level)]
         
-        return dfs(0,0)
+        # return dfs(0,0)
+
+        # Bottom Up approach
+        for i in range(len(triangle) -2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+        
+        return triangle[0][0]
