79f98d2e18931aa394038dcb3d631905027060db
Manmath Jukale <jukalemanmath@gmail.com>
2025-09-26 18:56:38 -0400

    Time: 11 ms (30.13%), Space: 20.9 MB (64.13%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,19 +1,30 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if not intervals:
-            return [[]]
-        intervals.sort()
-        res = []
-        start, end = intervals[0]
-        for i in range(1, len(intervals)):
-            if end < intervals[i][0]:
-                res.append([start, end])
-                start, end = intervals[i]
-            else:
-                if end >= intervals[i][0]:
-                    start = min(start, intervals[i][0])
-                    end = max(end , intervals[i][1])
+        # if not intervals:
+        #     return [[]]
+        # intervals.sort()
+        # res = []
+        # start, end = intervals[0]
+        # for i in range(1, len(intervals)):
+        #     if end < intervals[i][0]:
+        #         res.append([start, end])
+        #         start, end = intervals[i]
+        #     else:
+        #         if end >= intervals[i][0]:
+        #             start = min(start, intervals[i][0])
+        #             end = max(end , intervals[i][1])
 
-        res.append([start,end])
+        # res.append([start,end])
+        # return res
+
+        intervals.sort()
+        res=[intervals[0]]
+        for i in range(1, len(intervals)):
+            if res[-1][1] < intervals[i][0]:
+                res.append(intervals[i])
+            else:
+                start, end = res.pop()
+                res.append([min(start, intervals[i][0]), max(end, intervals[i][1])])
         return res
+
         
