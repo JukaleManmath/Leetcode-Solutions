79812fd1295fd344ca6beed03725b2aa68f411ab
Manmath Jukale <jukalemanmath@gmail.com>
2025-09-29 20:19:14 -0400

    Time: 2 ms (35.46%), Space: 18 MB (34.68%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,17 @@
 class Solution:
-    def matches(self, ch1, ch2):
-        matche = {
-            ')':'(',
-            "}":"{",
-            "]":"[",
-        }
-        return matche[ch1] == ch2
-
     def isValid(self, s: str) -> bool:
-        st = deque()
-        for ch in s:
-            if ch == "(" or ch == "{" or ch == "[":
-                st.append(ch)
-            if ch == ")" or ch == "}" or ch == "]":
-                if len(st) == 0:
-                    return False
-                if not self.matches(ch, st.pop()):
-                    return False
-        return len(st) == 0
+        mp = {
+            ")" : "(",
+            "]": "[",
+            "}":"{"
+        }
+        stack = []
+        for i in range(len(s)):
+            if s[i] in mp:
+                if stack and mp[s[i]] == stack[-1]:
+                     stack.pop()
+                else:
+                    return False 
+            else:    
+                stack.append(s[i])
+        return len(stack) == 0
