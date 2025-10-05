89b7b31613a009f18d3f1da8c51ac5afa47d7f80
Manmath Jukale <jukalemanmath@gmail.com>
2025-10-05 17:40:33 -0400

    Time: 4 ms (71.24%), Space: 21.3 MB (89.5%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -15,7 +15,12 @@ class MinStack:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.minheap[-1]
+        while self.minheap:
+            minele = self.minheap[0]
+            if minele in self.stack:
+                return minele
+            else:
+                heapq.heappop(self.minheap)
             
             
         
