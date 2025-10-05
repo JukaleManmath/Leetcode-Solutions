69297f9b129a5db064559411b22f8f70e3fa5f60
Manmath Jukale <jukalemanmath@gmail.com>
2025-10-05 17:39:25 -0400

    Time: 7 ms (50.72%), Space: 21.4 MB (34.28%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,22 +2,28 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.minstack = []
+        self.minheap = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        val = min(val, self.minstack[-1]) if self.minstack else val
-        self.minstack.append(val)
+        heapq.heappush(self.minheap, val)
 
     def pop(self) -> None:
         self.stack.pop()
-        self.minstack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.minstack[-1]
+        while self.minheap:
+            minele = self.minheap[0]
+            if minele in self.stack:
+                return minele
+            else:
+                heapq.heappop(self.minheap)
+            
+            
+        
 
 
 # Your MinStack object will be instantiated and called as such:
