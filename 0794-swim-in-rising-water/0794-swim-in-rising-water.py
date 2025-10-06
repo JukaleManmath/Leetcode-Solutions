4e1c1af737fa3724e4814205347615486ce9f3d2
Manmath Jukale <jukalemanmath@gmail.com>
2025-10-05 20:52:50 -0400

    Time: 20 ms (90.19%), Space: 18.5 MB (38.11%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -1,19 +1,17 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        # Djikistras algo
         n = len(grid)
-        minheap = [(grid[0][0], 0 , 0)]
+        directions = [[1, 0], [-1, 0], [0, 1],[0, -1]]
+        minheap = [[grid[0][0], 0 , 0]]
         visited = set([(0,0)])
-        direction = [[1,0],[-1,0],[0,1],[0,-1]]
         while minheap:
             time , i , j = heapq.heappop(minheap)
             if i == n-1 and j == n-1:
                 return time
-            for di, dj in direction:
-                ni, nj = di + i, dj + j
-                if ni >= 0 and nj >= 0 and ni <= n-1 and nj <= n-1 and (ni,nj) not in visited:
-                    visited.add((ni,nj))
-                    heapq.heappush(minheap,(max(time, grid[ni][nj]),ni, nj ))
+            for x , y in directions:
+                dx, dy = x + i, y + j
+                if 0 <= dx <n and 0 <= dy <n and (dx,dy) not in visited:
+                    visited.add((dx,dy))
+                    heapq.heappush(minheap, [max(time, grid[dx][dy]), dx, dy])
         return -1
-            
-            
+
