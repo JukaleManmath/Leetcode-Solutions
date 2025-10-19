358b6bd7582bde49235b17936b1458bc45ceae12
Manmath Jukale <jukalemanmath@gmail.com>
2025-10-19 12:13:16 -0400

    Time: 25 ms (99.5%), Space: 18.5 MB (59.73%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -11,19 +11,30 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if head is None:
             return
-        hash = {}
-        curr = head
-        new_head = Node(curr.val)
-        hash[curr] = new_head
-        while curr.next:
-            curr = curr.next
-            new_head.next = Node(curr.val)
-            new_head = new_head.next
-            hash[curr] = new_head
-        
+        dummy = head
+
+        while dummy:
+            newNode = Node(dummy.val)
+            newNode.next = dummy.next
+            dummy.next = newNode
+            dummy = dummy.next.next
+
         curr = head
         while curr:
-            temp = hash[curr]
-            temp.random = hash.get(curr.random)
-            curr = curr.next
-        return hash[head]
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
+        
+        dummy = head
+        new = head.next
+        while dummy:
+            copy = dummy.next
+            dummy.next = copy.next
+            if copy.next:
+                copy.next =copy.next.next
+            dummy = dummy.next
+        
+        return new
+
+        
+        
