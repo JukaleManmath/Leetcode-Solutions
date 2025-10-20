c8db14e115764861d52d66fab5b97bc419c0d2f3
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2025-10-20 19:49:44 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,68 +1,46 @@
 class Node:
-    def __init__(self, key: int, val: int):
+    def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.prev = self.right = None
+        self.prev = self.next = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        # Brute Force
-        # self.cache = []
-        # self.capacity = capacity
-
         self.capacity = capacity
-        self.cache = {} # HashMap for storing the nodes of Doubly linkedlist and getting operations in O(1) complexty
-
-        self.left , self.right = Node(0, 0), Node(0,0)
-        self.left.next , self.right.prev = self.right, self.left
-
-    def remove(self, node):
-        prev , nxt = node.prev, node.next
-        prev.next, nxt.prev= nxt, prev
-
+        self.cache = {}
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+    
     def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.prev, node.next = prev, nxt
+        prevnode , nex = self.right.prev , self.right
+        prevnode.next = nex.prev = node
+        node.prev, node.next = prevnode, nex
+    def remove(self, node):
+        prev , nex = node.prev, node.next
+        prev.next = nex
 
     def get(self, key: int) -> int:
-        # BF
-        # for i in range(len(self.cache)):
-        #     if self.cache[i][0] == key:
-        #         tmp = self.cache.pop(i)
-        #         self.cache.append(tmp)
-        #         return tmp[1]
-        # return -1
-
         if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
+            node = self.cache[key]
+            self.remove(node)
+            self.insert(node)
+            return node.val
         return -1
+        
 
     def put(self, key: int, value: int) -> None:
-        #BF
-        # for i in range(len(self.cache)):
-        #     if self.cache[i][0] == key:
-        #         tmp = self.cache.pop(i)
-        #         tmp[1] = value
-        #         self.cache.append(tmp)
-        #         return 
-
-        # if self.capacity == len(self.cache):
-        #     self.cache.pop(0)
-        
-        # self.cache.append([key, value])
-
         if key in self.cache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
-
         if len(self.cache) > self.capacity:
-            lru = self.left.next
-            self.remove(self.cache[lru.key])
-            del self.cache[lru.key]
+            node = self.left.next
+            self.remove(node)
+            del self.cache[node.key]
+        
+
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
