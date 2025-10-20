a15cd91000e71f1544b7edb4e2cea704907ae006
Manmath Jukale <jukalemanmath@gmail.com>
2025-10-19 22:32:07 -0400

    Time: 0 ms (100%), Space: 18.6 MB (58.15%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,54 +5,40 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # O(n) -time but space also - O(n)
-        # arr = []
-        # while head:
-        #     arr.append(head)
-        #     head = head.next
-        
-        # n = len(arr)
-        # i = 0
-        # while i+k <= len(arr):
-        #     left = i
-        #     right = i + k - 1
-        #     while left <= right:
-        #         arr[left], arr[right] = arr[right], arr[left]
-        #         left+= 1
-        #         right -= 1
-        #     i += k
-            
-        # for l in range(len(arr)-1):
-        #     arr[l].next = arr[l+1]
-        # arr[n-1].next = None
-        # return arr[0]
+        if not head or k == 1:
+            return head
 
+        dummy = ListNode(0)
+        dummy.next = head
 
-        # O(n) - time and. O(1) -> space
-        dummy = ListNode(0, head)
-        groupPrev = dummy 
-
-        while True:
-            kth = self.getKth(groupPrev, k)
-            if kth is None:
-                break
-            groupNext = kth.next
-
-            prev, curr = kth.next, groupPrev.next
-            while curr != groupNext:
-                temp = curr.next
-                curr.next = prev
-                prev = curr
-                curr =temp
-            
-            tmp = groupPrev.next
-            groupPrev.next = kth
-            groupPrev = tmp
-
-        return dummy.next
-            
-    def getKth(self, curr, k):
-        while curr and k > 0:
+        n = 0
+        curr = head
+        while curr:
+            n += 1
             curr = curr.next
-            k -= 1
-        return curr
+        
+        loop = n // k
+
+        curr = head
+        prev = dummy
+     
+        for _ in range(loop):
+            tail = curr
+            prev_node = None
+
+            for _ in range(k):
+                temp = curr.next
+                curr.next = prev_node
+                prev_node = curr
+                curr = temp
+            
+            tail.next = curr
+            prev.next = prev_node
+            prev = tail
+        return  dummy.next
+
+
+
+        
+
+            
