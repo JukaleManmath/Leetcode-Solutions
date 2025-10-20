0b4e2edef847b81b902d9c9fa7f624615ee8b81f
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2025-10-20 00:11:22 -0400

    Time: 0 ms (100%), Space: 17.9 MB (34.18%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -8,20 +8,32 @@ class Solution:
         if not head:
             return head
         
-        N = 0
-        curr = head
-        while curr:
-            N += 1
-            curr = curr.next
+        # N = 0
+        # curr = head
+        # while curr:
+        #     N += 1
+        #     curr = curr.next
         
+        # dummy = ListNode(0, head)
+        # curr = head
+        # prev = dummy
+        # for i in range(N- n):
+        #     prev = curr
+        #     curr =curr.next
         
-        dummy = ListNode(0, head)
-        curr = head
-        prev = dummy
-        for i in range(N- n):
-            prev = curr
-            curr =curr.next
+        # prev.next = curr.next
+        # return dummy.next
 
+        # two pointers approach
+        dummy = ListNode(0, head)
+        ahead , behind = dummy, dummy
+
+        for _ in range(n + 1):
+            ahead = ahead.next
         
-        prev.next = curr.next
+        while ahead:
+            ahead = ahead.next
+            behind = behind.next
+        
+        behind.next = behind.next.next
         return dummy.next
