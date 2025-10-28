00c0d68ac7def0f9f2927432088f86ba66f2f31a
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2025-10-27 23:04:25 -0400

    Time: 1687 ms (17.68%), Space: 215.6 MB (19.86%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.py
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.py
@@ -3,26 +3,23 @@ class Solution:
         n , m = len(isWater), len(isWater[0])
         direction = [[-1,0], [1, 0], [0,1],[0,-1]]
         res = [[0 for _ in range(m)] for _ in range(n) ]
-        visited = {}
-        def solve(q, h):
-            while q:
-                for _ in range(len(q)):
-                    row, col = q.popleft()
-                    res[row][col] = h
-                    for dr , dc in direction:
-                        nr , nc = row + dr , col + dc
-                        if (nr,nc) not in visited and 0 <= nr < n and 0 <= nc < m:
-                            q.append([nr,nc])
-                            visited[(nr, nc)] = True
-                h += 1
-        
+        visited = set()
         q = deque()
         for i in range(n):
             for j in range(m):
                 if isWater[i][j] == 1:
                     q.append([i, j])
-                    visited[(i, j)] = True
-        
-        solve(q, 0)
+                    visited.add((i,j))
+        h = 0
+        while q:
+            for _ in range(len(q)):
+                row, col = q.popleft()
+                res[row][col] = h
+                for dr , dc in direction:
+                    nr , nc = row + dr , col + dc
+                    if (nr,nc) not in visited and 0 <= nr < n and 0 <= nc < m:
+                        q.append([nr,nc])
+                        visited.add((nr, nc))
+            h += 1
 
         return res
