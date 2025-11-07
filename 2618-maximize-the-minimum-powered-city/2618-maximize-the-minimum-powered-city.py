bde2a8a8b28294ec5ff4a903b0e1f6c38d06b23a
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2025-11-07 02:08:29 -0500

    Time: 1602 ms (44.33%), Space: 30.2 MB (61.86%) - LeetHub

---
+++ 2618-maximize-the-minimum-powered-city/2618-maximize-the-minimum-powered-city.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxPower(self, stations: List[int], r: int, k: int) -> int:
+        left , right = 0 , 10**12
+        res = min(stations)
+        n = len(stations)
+
+        def ispossible(minpower, s, k):
+            nonlocal n
+            curr = 0
+            le ,ri = 0 , r
+            for i in range(r+ 1):
+                curr += s[i]
+            
+            for i in range(n):
+                if i - le > r:
+                    curr -= s[le]
+                    le += 1
+                if ri - i < r and ri< n-1:
+                    ri += 1
+                    curr += s[ri]
+                
+                if curr < minpower:
+                    power_needed = minpower - curr
+                    if k < power_needed: return False
+                    s[ri] += power_needed
+                    curr += power_needed
+                    k -= power_needed    
+            return True
+        
+        while left <= right:
+            mid = (left + right) // 2
+            print(mid)
+            if ispossible(mid, stations[:], k):
+                left = mid + 1
+                res = mid     
+            else:
+                right = mid - 1
+        return res
+
