3bf05489f4ce8d7fa14eccbb397bf8c1f8f15eb1
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2025-11-12 20:46:15 -0500

    Time: 59 ms (67.07%), Space: 18.1 MB (81.44%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.py
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.py
@@ -4,16 +4,11 @@ class Solution:
         n = len(s)
         i = 0
         num1 = 0
-        while i < n - 1:
-            if s[i] == "1" and s[i+ 1] == "0":
-                i += 1
-                num1 += 1
+        while i < n:
+            if s[i] == "0":
+                res += num1
                 while i < n and s[i] != "1":
                     i += 1
-                res += num1
-            elif s[i] == "1" and s[i+1] == "1":
-                i += 1
-                num1 += 1
-            else:
-                i += 1
+            num1 += 1
+            i += 1
         return res
