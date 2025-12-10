ece2395166ed9bfbec58874d804ce27783e42836
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2025-12-09 21:30:38 -0500

    Time: 3132 ms (5.55%), Space: 31.8 MB (82.78%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
@@ -3,16 +3,10 @@ class Solution:
         n = len(complexity)
         smallest = complexity[0]
         mod = 10 ** 9 + 7
-
-        mp = defaultdict(int)
         for i in range(1, n):
             if complexity[i] <= smallest:
                 return 0
-            mp[complexity[i]] += 1
         
-        div = 1
-        for i in mp:
-            div *= mp[i]
         ans = 1
         for i in range(1, n):
             ans *= i
