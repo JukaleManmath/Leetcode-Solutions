a3da90606cb76c3197f15cf805a1c1c990b4749f
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-01-24 18:55:22 -0500

    Time: 18 ms (35.48%), Space: 24.1 MB (8.1%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,21 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mp = {}
+        # naive with sorting
+        # mp = {}
 
-        n = len(strs)
-        for i in range(n):
-            curr = "".join(sorted(strs[i]))
-            if curr in mp:
-                mp[curr].append(strs[i])
-            else:
-                mp[curr] = [strs[i]]
-        return list(mp.values())
+        # n = len(strs)
+        # for i in range(n):
+        #     curr = "".join(sorted(strs[i]))
+        #     if curr in mp:
+        #         mp[curr].append(strs[i])
+        #     else:
+        #         mp[curr] = [strs[i]]
+        # return list(mp.values())
+
+        res = defaultdict(list)
+        for i in range(len(strs)):
+            cnt = [0] * 26
+            for c in strs[i]:
+                cnt[ord(c) - ord('a')] += 1
+            res[tuple(cnt)].append(strs[i])
+        return list(res.values())
