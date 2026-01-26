ececfc5f5ba4acdb3f530649e94cc10363fd6ce7
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-01-26 07:38:31 -0500

    Time: 49 ms (69.81%), Space: 31.2 MB (42.06%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -9,6 +9,5 @@ class Solution:
                 mindiff = arr[i + 1] - arr[i]
             elif arr[i+1] - arr[i] == mindiff:
                 res.append([arr[i], arr[i+1]])
-            print(res)
             
         return res
