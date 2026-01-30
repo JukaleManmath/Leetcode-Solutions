f200c144193b25a8c579d521bae4ee5b4d537983
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-01-30 12:13:51 -0500

    Time: 40 ms (68.21%), Space: 19.3 MB (8.45%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,14 +1,27 @@
 class Solution:
     def fib(self, n: int) -> int:
-        dp = [-1] * (n+1)
-        def rec(n):
-            if n <=1:
-                return n
+        # memoization -> top -down approach. from answer -> base case
+        # dp = [-1] * (n+1)
+        # def rec(n):
+        #     if n <=1:
+        #         return n
             
-            if dp[n] != -1:
-                return dp[n]
+        #     if dp[n] != -1:
+        #         return dp[n]
             
-            dp[n] = rec(n-1) + rec(n-2)
-            return dp[n]
+        #     dp[n] = rec(n-1) + rec(n-2)
+        #     return dp[n]
         
-        return rec(n)
+        # return rec(n)
+
+        # tabulation ->. bottom up -> from base case to answer
+        if n <= 1:
+            return n
+        dp = [-1] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
