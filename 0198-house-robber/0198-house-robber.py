1abaaefdc0c420635bbdfd901d6d18f8c50e36de
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-01 10:58:41 -0500

    Time: 0 ms (100%), Space: 19.3 MB (42.38%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,16 +1,30 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         # Memoization
-        n = len(nums)
-        dp = [-1] * (n + 1)
+        # n = len(nums)
+        # dp = [-1] * (n + 1)
         
-        #recursion
-        def rec(n):
-            if n < 0:
-                return 0
-            if dp[n] != -1:
-                return dp[n]
+        # #recursion
+        # def rec(n):
+        #     if n < 0:
+        #         return 0
+        #     if dp[n] != -1:
+        #         return dp[n]
 
-            dp[n] = max(rec(n-2) + nums[n] , rec(n-1))
-            return dp[n]
-        return rec(len(nums) - 1)
+        #     dp[n] = max(rec(n-2) + nums[n] , rec(n-1))
+        #     return dp[n]
+        # return rec(len(nums) - 1)
+
+        # Tabulation -> Bottom -up
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        n = len(nums)
+        dp = [-1] * (n)
+        dp[0] = nums[0]
+        dp[1] = max(dp[0], nums[1])
+
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+        return dp[n-1]
