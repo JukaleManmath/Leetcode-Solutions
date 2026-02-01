4273b17f2194958e14c67fb7423f901bfc910190
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-01 11:45:50 -0500

    Time: 0 ms (100%), Space: 19.5 MB (12.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -16,15 +16,30 @@ class Solution:
         # return rec(len(nums) - 1)
 
         # Tabulation -> Bottom -up
-        if not nums:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
-        n = len(nums)
-        dp = [-1] * (n)
-        dp[0] = nums[0]
-        dp[1] = max(dp[0], nums[1])
+        # if not nums:
+        #     return 0
+        # if len(nums) == 1:
+        #     return nums[0]
+        # n = len(nums)
+        # dp = [-1] * (n)
+        # dp[0] = nums[0]
+        # dp[1] = max(dp[0], nums[1])
 
-        for i in range(2, n):
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
-        return dp[n-1]
+        # for i in range(2, n):
+        #     dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+        # return dp[n-1]
+
+        # space optimization
+        n = len(nums)
+        if n == 0:
+            return 0
+        prev, prev2 = nums[0], 0
+        for i in range(1, n):
+            take = nums[i] + prev2
+            notake = prev
+            curr = max(take, notake)
+            prev2 = prev
+            prev = curr
+        
+        return prev
+            
