ce56bac5930a5577291f7d715443e25186b2e3cb
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-06 20:27:30 -0500

    Time: 130 ms (93.15%), Space: 20.2 MB (47.95%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,12 +1,21 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        stack = []
-        cnt = 0
+        # stack = []
+        # cnt = 0
 
+        # for c in s:
+        #     if stack and stack[-1] == "b" and c == "a":
+        #         stack.pop()
+        #         cnt += 1
+        #     else:
+        #         stack.append(c)
+        # return cnt
+
+        cnt, res = 0, 0 
         for c in s:
-            if stack and stack[-1] == "b" and c == "a":
-                stack.pop()
+            if c == "b":
                 cnt += 1
-            else:
-                stack.append(c)
-        return cnt
+            elif cnt:
+                res += 1
+                cnt -= 1
+        return res
