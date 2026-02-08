4103ac84ef8c230a69f704bc54353fe8031e1419
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-08 00:55:53 -0500

    Time: 0 ms (100%), Space: 20.7 MB (5.56%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -5,33 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    #  Brute force - O(n**2)
-    # def maxHeight(self, root):
-    #     if root is None:
-    #         return 0
-    #     return 1 + max(self.maxHeight(root.left), self.maxHeight(root.right))
-    # def isBalanced(self, root: Optional[TreeNode]) -> bool:
-    #     if root is None:
-    #         return True
-    #     left = self.maxHeight(root.left)
-    #     right = self.maxHeight(root.right)
-    #     if (left > right and left - right > 1 ) or (right > left and right - left > 1):
-    #         return False
-        
-    #     return self.isBalanced(root.left) and self.isBalanced(root.right)
-
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         flag = True
-
         def dfs(root):
             nonlocal flag
             if root is None:
-                return 0
-            left = dfs(root.left)
-            right = dfs(root.right)
-            if abs(left - right) > 1:
+                return 0        
+            l = dfs(root.left)
+            r = dfs(root.right)
+            if abs(l - r) > 1:
                 flag = False
-            return 1 + max(left, right)
-        
+            return 1 + max(l, r)
         dfs(root)
         return flag
