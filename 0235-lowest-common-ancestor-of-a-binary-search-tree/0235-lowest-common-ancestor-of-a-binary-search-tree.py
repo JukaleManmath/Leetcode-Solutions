b3a0e99c491ecef6a7ee1b5c05cf54786f71f0ec
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-09 01:56:06 -0500

    Time: 63 ms (49.6%), Space: 22.8 MB (40.33%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,24 +7,14 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # Recursive dfs approach - O(h) time and space O(h) - h height of the tree
-        # if not root or not p or not p:
-        #     return None
-        # max_val = max(p.val, q.val)
-        # min_val = min(p.val, q.val)
+        if not root or not p or not q:
+            return None
+        
+        if (max(p.val, q.val) < root.val):
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif (min(p.val, q.val) > root.val):
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
 
-        # if root.val <= max_val and root.val >= min_val:
-        #     return root
-        # if root.val < min_val:
-        #     return self.lowestCommonAncestor(root.right, p , q)
-        # return self.lowestCommonAncestor(root.left, p , q)
-
-        # Iterative approach - time complexity O(h) , space complexity o(1)
-        curr = root
-        while curr:
-            if curr.val < p.val and curr.val < q.val:
-                curr = curr.right
-            elif curr.val > p.val and curr.val > q.val:
-                curr = curr.left
-            else:
-                return curr 
+        
