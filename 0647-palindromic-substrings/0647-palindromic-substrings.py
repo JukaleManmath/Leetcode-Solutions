2730af63b8312b00d0e0f410a2edf629faa4c7a3
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-12 01:21:32 -0500

    Time: 183 ms (31.49%), Space: 27.2 MB (13.67%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -8,18 +8,28 @@ class Solution:
         #             cnt += 1
         # return cnt
 
-        cnt = 0
-        for i in range(len(s)):
-            l = i
-            r = i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                cnt += 1
-                l -= 1
-                r += 1
+        # cnt = 0
+        # for i in range(len(s)):
+        #     l = i
+        #     r = i
+        #     while l >= 0 and r < len(s) and s[l] == s[r]:
+        #         cnt += 1
+        #         l -= 1
+        #         r += 1
             
-            l , r = i , i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                cnt += 1
-                l -= 1
-                r += 1
+        #     l , r = i , i + 1
+        #     while l >= 0 and r < len(s) and s[l] == s[r]:
+        #         cnt += 1
+        #         l -= 1
+        #         r += 1
+        # return cnt
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        cnt = 0
+
+        for i in range(n-1,-1, -1):
+            for j in range(i , n):
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+                    cnt += 1
         return cnt
