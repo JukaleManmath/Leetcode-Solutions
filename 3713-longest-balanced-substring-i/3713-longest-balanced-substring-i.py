8dfa170d00b396fe6b83fbe4b66a06125f20c28c
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-11 19:37:01 -0500

    Time: 2881 ms (42.96%), Space: 19.4 MB (20.27%) - LeetHub

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        n = len(s)
+        res = 0
+        for l in range(n):
+            mp = defaultdict(int)
+            for r in range(l , n):
+                mp[s[r]] += 1
+                if max(mp.values()) == mp[s[r]] and min(mp.values()) == mp[s[r]]:
+                    if r - l + 1 > res:
+                        res = r - l + 1
+        return res
