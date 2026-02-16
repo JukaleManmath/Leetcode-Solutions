f92517fcfbb8b323ce266fa1bad34b33bd24db26
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-16 01:07:42 -0500

    Time: 3 ms (76.65%), Space: 20.8 MB (15.21%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -2,14 +2,12 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
         dp = [[float("inf") for _ in range(i + 1)] for i in range(n)]
-        def rec(i, j):
-            if dp[i][j] != float("inf"):
-                return dp[i][j]
-            
-            if i == n-1:
-                return triangle[i][j]
-            down = triangle[i][j] + rec(i + 1, j)
-            diag = triangle[i][j] + rec(i + 1, j + 1)
-            dp[i][j] = min(down, diag)
-            return dp[i][j]
-        return rec(0, 0)
+    
+        for i in range(n-1, -1, -1):
+            for j in range(i + 1):
+                if i == n -1:
+                   dp[i][j] = triangle[i][j]
+                   continue
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j] , dp[i + 1][j + 1])
+
+        return dp[0][0]
