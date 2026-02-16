c10d27bd1d8f31b25295de117b2304056349c861
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-16 00:20:35 -0500

    Time: 5 ms (97.74%), Space: 21.7 MB (38.24%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,22 +1,17 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m , n = len(grid), len(grid[0])
-        dp = [[-1] * n for _ in range(m)]
+        dp = [[float("inf")] * n for _ in range(m)]
 
-        def rec(i, j):
-            if i == 0 and j == 0:
-                return grid[i][j]
-            if dp[i][j] != -1:
-                return dp[i][j]
-            
-            up , left = float("inf") , float("inf")
-            if i > 0:
-                up = rec(i-1, j)
-            if j > 0:
-                left = rec(i, j -1)
-            
-            dp[i][j] =  grid[i][j] + min(up, left)
-            return dp[i][j]
-        return rec(m-1, n-1)
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[i][j]
+                    continue
+                
+                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
+        
+        return dp[m-1][n-1]
+
             
             
