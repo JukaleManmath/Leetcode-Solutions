8e97b989489011bb56cd4559cb070cedea9bba2b
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-16 00:56:26 -0500

    Time: 15 ms (7.94%), Space: 21 MB (10.89%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,24 +1,15 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        #  Recursive approach
-        # n = len(triangle)
-        # path = {}
-
-        # def dfs(i , level):
-        #     if level == n -1:
-        #         return triangle[level][i]
-        #     if (i,level) in path:
-        #         return path[(i,level)]
+        n = len(triangle)
+        dp = [[float("inf") for _ in range(i + 1)] for i in range(n)]
+        def rec(i, j):
+            if dp[i][j] != float("inf"):
+                return dp[i][j]
             
-        #     path[(i,level)]= min(triangle[level][i] + dfs(i , level + 1),
-        #                 triangle[level][i] + dfs(i + 1 , level + 1))
-        #     return path[(i,level)]
-        
-        # return dfs(0,0)
-
-        # Bottom Up approach
-        for i in range(len(triangle) -2, -1, -1):
-            for j in range(len(triangle[i])):
-                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
-        
-        return triangle[0][0]
+            if i == n-1:
+                return triangle[i][j]
+            down = triangle[i][j] + rec(i + 1, j)
+            diag = triangle[i][j] + rec(i + 1, j + 1)
+            dp[i][j] = min(down, diag)
+            return dp[i][j]
+        return rec(0, 0)
