3a583ab73c5918dafd5da02ca6d76aba049c69f4
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-16 02:01:30 -0500

    Time: 2775 ms (5.37%), Space: 178.5 MB (9.9%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,26 +1,23 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if not nums:
-            return True
-        s = 0
-        for num in nums:
-            s += num
-        if s % 2 != 0:
+        n = len(nums)
+        Sum = sum(nums)
+        if Sum % 2 != 0:
             return False
-        target = s // 2
+        target = Sum // 2
 
-        t = [[False] * (target + 1) for i in range(len(nums)+1)]
-        for i in range(len(nums) + 1):
-            for j in range(target + 1):
-                if j == 0:
-                    t[i][j] = True
+        dp = [[-1 for _ in range(target + 1)] for _ in range(n)]
+        def rec(i,  tar):
+            if tar == 0:
+                return True
+            if i >= n:
+                return False
+            if dp[i][tar] != -1:
+                return dp[i][tar]
+            
+            take = rec(i + 1, tar - nums[i])
+            notake = rec(i + 1, tar)
 
-        
-        for i in range(1, len(nums)+1):
-            for j in range(1, target + 1):
-                if nums[i-1] <= j:
-                    t[i][j] = t[i-1][j- nums[i-1]] or t[i-1][j]
-                else:
-                    t[i][j] = t[i-1][j]
-        
-        return t[len(nums)][target]
+            dp[i][tar] =  take or notake
+            return dp[i][tar]
+        return rec(0, target)
