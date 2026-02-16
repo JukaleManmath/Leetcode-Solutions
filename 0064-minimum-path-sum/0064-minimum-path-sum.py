45194a6d62b277fcd9a62afbee641538cac2b239
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-16 00:17:32 -0500

    Time: 39 ms (14.5%), Space: 23.3 MB (14.66%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,16 +1,22 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        n , m = len(grid), len(grid[0])
-        dp = [[0 for _ in range(m )] for _ in range(n)]
-        for i in range(n):
-            for j in range(m):
-                if not i -1 >= 0:
-                    dp[i][j] = dp[i][j - 1] + grid[i][j]
-                elif not j-1>= 0:
-                    dp[i][j] = dp[i-1][j] + grid[i][j]
-                elif not (i-1>= 0 and j- 1 >= 0):
-                    dp[i][j] = grid[i][j]
-                else:
-                    dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + grid[i][j]
+        m , n = len(grid), len(grid[0])
+        dp = [[-1] * n for _ in range(m)]
+
+        def rec(i, j):
+            if i == 0 and j == 0:
+                return grid[i][j]
+            if dp[i][j] != -1:
+                return dp[i][j]
             
-        return dp[n-1][m-1]
+            up , left = float("inf") , float("inf")
+            if i > 0:
+                up = rec(i-1, j)
+            if j > 0:
+                left = rec(i, j -1)
+            
+            dp[i][j] =  grid[i][j] + min(up, left)
+            return dp[i][j]
+        return rec(m-1, n-1)
+            
+            
