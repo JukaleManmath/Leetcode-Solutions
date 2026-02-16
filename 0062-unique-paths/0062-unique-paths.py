2211602d108f154fcce706a70ac6febf4c99b9c3
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-15 20:11:04 -0500

    Time: 4 ms (7.19%), Space: 19.6 MB (10.5%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,22 +1,19 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        #  memoization
-        # memo = [[-1] * (n) for _ in range(m)]
-        # def dfs(i, j):
-        #     if i >= m or j >= n:
-        #         return 0
-        #     if memo[i][j] != -1:
-        #         return memo[i][j]
-        #     if i == m-1 and j == n - 1:
-        #         return 1
-        #     memo[i][j] = dfs(i +1,j) + dfs(i, j + 1)
-        #     return memo[i][j]
-        # return dfs(0, 0)
+        dp = [[-1] * n for _ in range(m)]
+        def rec(i, j ):
+            if i == 0 and j == 0:
+                return 1
+            
+            if i < 0 or j < 0:
+                return 0
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
 
-        # Bottom-up
-        dp = [[0] * (n + 1) for _ in range(m+1)]
-        dp[m-1][n-1] = 1
-        for i in range(m-1,-1,-1):
-            for j in range(n-1,-1,-1):
-                dp[i][j] += dp[i + 1][j] + dp[i][j + 1]
-        return dp[0][0]
+            up = rec(i-1, j)
+            left = rec(i, j -1)
+            dp[i][j] =  up + left
+            return dp[i][j]
+        return rec(m-1, n -1)
+    
