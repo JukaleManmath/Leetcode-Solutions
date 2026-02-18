04141117436e31c87f877d624e587b3519dadcf2
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-17 20:13:55 -0500

    Time: 0 ms (100%), Space: 19.3 MB (65.59%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
@@ -1,12 +1,15 @@
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        flag = n & 1
-        n = n >> 1
-        while n:
-            curr = n & 1
-            if curr == flag:
-                return False
-            flag = curr
-            n = n >> 1
+        # flag = n & 1
+        # n = n >> 1
+        # while n:
+        #     curr = n & 1
+        #     if curr == flag:
+        #         return False
+        #     flag = curr
+        #     n = n >> 1
         
-        return True
+        # return True
+
+        x = n ^ (n >> 1)
+        return (x & (x + 1)) == 0
