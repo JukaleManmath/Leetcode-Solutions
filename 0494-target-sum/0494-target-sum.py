fb88ebbc44129825d3d918a5358586d2c7575444
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-19 22:59:39 -0500

    Time: 43 ms (80.77%), Space: 23 MB (43.4%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,14 +1,26 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        #options - add or subtract
-        dp = {}
-        def dfs(i, res):
-            if i == len(nums):
-                return res == target
-            if (i,res) in dp:
-                return dp[(i,res)]
-            dp[(i,res)] =  dfs(i + 1 , res + nums[i]) + dfs(i+1, res - nums[i])
-            return dp[(i,res)]
-
-        return dfs(0, 0)
+        n = len(nums)
+        
+        total = sum(nums)
+        if total - target < 0 or (total - target)% 2 != 0:
+            return 0
+        new = (total - target) // 2
+        dp = [[-1] *(new + 1) for _ in range(n)]
+        def rec(i , t):
+            if i == 0:
+                if t == 0 and nums[0] == 0:
+                    return 2
+                if t == 0 or nums[i] == t:
+                    return 1
+                return 0
             
+            if dp[i][t] != -1:
+                return dp[i][t]
+            nopick = rec(i - 1, t)
+            pick = 0
+            if nums[i] <= t:
+                pick = rec(i - 1, t - nums[i])
+            dp[i][t] =  nopick + pick
+            return dp[i][t]
+        return rec(n-1, new)
