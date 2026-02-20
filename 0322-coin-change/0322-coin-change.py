6318118abd279a42e363eaf3f8cdbc9be255a0b1
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-19 20:04:33 -0500

    Time: 939 ms (20.37%), Space: 20.1 MB (33.87%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,21 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        dp = [[0] * (amount + 1) for _ in range(n)]
-        
+        prev , curr = [0] * (amount + 1), [0] *(amount + 1)
+
         for i in range(amount + 1):
             if i % coins[0] == 0:
-                dp[0][i] = i // coins[0]
+                prev[i] = i // coins[0]
             else:
-                dp[0][i] = 1e5
-
-        for i in range(1, n):
-            for j in range(0, amount + 1):
-                notake = dp[i-1][j]
+                prev[i] = float("inf")
+        
+        for i in range(1,n):
+            for j in range(amount + 1):
+                notake = prev[j]
                 take = float("inf")
                 if coins[i] <= j:
-                    take = 1 + dp[i][j - coins[i]]
-                dp[i][j] = min(take, notake)
+                    take = 1 + curr[j - coins[i]]
+                curr[j] = min(take, notake)
+            prev = curr
         
-        return dp[n-1][amount] if dp[n-1][amount] != 1e5 else -1
-        
+        ans = prev[amount]
+        if ans == float("inf"):
+            return -1
+        return ans
