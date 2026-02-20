c2015b0923be207da261aec18f74379366a9846f
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-19 19:28:28 -0500

    Time: 947 ms (19.81%), Space: 86.6 MB (5.07%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,27 +1,22 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # memo = {}
-        # def dfs(amount):
-        #     if amount == 0:
-        #         return 0
-        #     if amount in memo:
-        #         return memo[amount]
-        #     res = float("inf")
-        #     for coin in coins:
-        #         if amount - coin >= 0:
-        #             res = min(res, 1 + dfs(amount- coin))
-        #     memo[amount] = res
-        #     return res
-        # mincoins = dfs(amount)
-        # return -1 if mincoins == float("inf") else mincoins
-
-        #  Bottom- up approach
-        inf = float("inf")
-        dp  = [inf] * (amount + 1)
-        dp[0] = 0
-        for i in range(1,amount+1):
-            for coin in coins:
-                if i - coin >= 0:
-                    dp[i] = min(dp[i], 1 + dp[i-coin])
-        return dp[amount] if dp[amount] != inf else -1
-        
+        n = len(coins)
+        dp = [[-1] * (amount + 1) for _ in range(n)]
+        def rec(ind, t):
+            if t == 0:
+                return 0
+            if ind == 0:
+                return t//coins[0] if t % coins[0] == 0 else float("inf")
+            if dp[ind][t] != -1:
+                return dp[ind][t]
+            
+            notake = rec(ind - 1, t)
+            take = float("inf")
+            if t >= coins[ind]:
+                take = 1 + rec(ind , t - coins[ind])
+            
+            dp[ind][t] =  min(notake, take)
+            return dp[ind][t]
+        ans =  rec(n - 1, amount) 
+        return ans if ans != float("inf") else -1
+            
