09326caaba1e469a4912f9e6b82f6f2cb229c793
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-02-23 20:45:42 -0500

    Time: 0 ms (100%), Space: 19.4 MB (82.49%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -6,21 +6,32 @@
 #         self.right = right
 class Solution:
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
-        paths= []
-        def dfs(root, path):
-            if root is None:
-                return
+        # paths= []
+        # def dfs(root, path):
+        #     if root is None:
+        #         return
             
-            path.append(str(root.val))
-            if root.left is None and root.right is None:
-                paths.append("".join(path[:]))
-            else:
-                dfs(root.left, path )
-                dfs(root.right, path)
-            path.pop()
-        dfs(root,[])
+        #     path.append(str(root.val))
+        #     if root.left is None and root.right is None:
+        #         paths.append("".join(path[:]))
+        #     else:
+        #         dfs(root.left, path )
+        #         dfs(root.right, path)
+        #     path.pop()
+        # dfs(root,[])
 
-        res = 0
-        for b in paths:
-            res += int(b, 2)
-        return res
+        # res = 0
+        # for b in paths:
+        #     res += int(b, 2)
+        # return res
+
+        def dfs(node, path):
+            if not node:
+                return 0
+            
+            path = (path << 1) + node.val
+            if not node.left and not node.right:
+                return path
+            return dfs(node.left, path) + dfs(node.right, path)
+        
+        return dfs(root, 0)
