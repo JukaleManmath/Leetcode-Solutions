be9afb1ee81a90da90c309c2ed2cca76ed05efa2
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-03-08 00:31:41 -0500

    Time: 0 ms (100%), Space: 19.7 MB (25.39%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        
+        res = ""
+        nums = set(nums)
+        n = len(nums)
+        def backtrack(i, res):
+            if i == n:
+                if res not in nums:
+                    return res[:]
+                else:
+                    return ""
+            res += "0"
+            ans = backtrack(i + 1, res)
+            if ans:
+                return ans
+            res = res[:-1]
+            res += "1"
+            ans = backtrack(i + 1, res)
+            if ans:
+                return ans
+        
+        return backtrack(0, "")
+            
