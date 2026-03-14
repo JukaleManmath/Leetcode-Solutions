ed6368d2b34599a5723c1ad564b353a7de85b185
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-03-13 20:19:47 -0400

    Time: 0 ms (100%), Space: 20.4 MB (65.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mp = {}
+        mp = defaultdict(int)
+
         for i in range(len(nums)):
-            tar = target - nums[i]
-            if nums[i] in mp:
-                return [mp[nums[i]], i]
-            mp[tar] = i
+
+            if target - nums[i] in mp:
+                return [mp[target - nums[i]] , i]
+            mp[nums[i]] = i
         
