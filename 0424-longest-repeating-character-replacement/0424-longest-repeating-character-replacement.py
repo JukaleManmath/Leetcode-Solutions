df2bda0580800ddc8bda490c6d96a42ab095b3a4
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-03-16 23:02:33 -0400

    Time: 155 ms (21.35%), Space: 19.7 MB (78.26%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,16 +3,12 @@ class Solution:
         n = len(s)
         mp = defaultdict(int)
         l = 0
-        maxf = 0
         res = 0
         for r in range(n):
             mp[s[r]] += 1
-            if mp[s[r]] > maxf:
-                maxf = mp[s[r]]
             
-            while (r - l + 1) - maxf > k:
+            while (r - l + 1) - max(mp.values()) > k:
                 mp[s[l]] -= 1
                 l += 1
-                maxf = max(maxf, mp[s[l]])
             res = max(res , r - l + 1)
         return res
