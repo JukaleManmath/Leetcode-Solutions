f7960faf91bf98074677c384535154e590bf254c
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-03-18 15:13:53 -0400

    Time: 0 ms (100%), Space: 22.5 MB (55.08%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -8,17 +8,15 @@ class MinStack:
         self.stack.append(val)
         val = min(val, self.minstack[-1]) if self.minstack else val
         self.minstack.append(val)
+
     def pop(self) -> None:
         self.stack.pop()
         self.minstack.pop()
-
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
         return self.minstack[-1]
-            
-        
 
 
 # Your MinStack object will be instantiated and called as such:
