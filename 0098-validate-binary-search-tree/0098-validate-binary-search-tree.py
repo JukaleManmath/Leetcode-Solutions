4b30f4034cfb7a10fac2dc7e17eecfdbf470a3e0
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-03-24 01:47:04 -0400

    Time: 0 ms (100%), Space: 21 MB (36.84%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,35 +6,15 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # DFS - the logic here is when from an root we move to the right in a BST all the values in the right
-        # should be greater the root and in the left should less than root
-        # so we set max_value while moving in left as root.val and min_val moving right as root.val
-        # recursively performing this logic gets the correct answer using DFS.
-        # def dfs(root, left, right):
-        #     if not root:
-        #         return True
+        def dfs(root, left, right):
+            if not root:
+                return True
             
-        #     if not( left < root.val < right):
-        #         return False
-            
-        #     return dfs(root.left, left, root.val) and dfs(root.right, root.val, right)
-        # return dfs(root, float("-inf"), float("inf"))
-            
-        # We can also perform this using BFS
-        q = deque()
-        q.append((root,float("-inf"), float("inf")))
-
-        while q:
-            node, left, right = q.popleft()
-
-            if not (left < node.val < right):
+            if not left < root.val < right:
                 return False
             
-            if node.left:
-                q.append((node.left, left , node.val))
-            if node.right:
-                q.append((node.right, node.val, right ))
-        return True
-
-
+            return dfs(root.left, left, root.val) and dfs(root.right, root.val, right)
+            
         
+        return dfs(root, float("-inf"), float("inf"))
+
