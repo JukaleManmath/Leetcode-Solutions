fcc093d83845fd380ba0371a20e4bec4c990a7f4
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-03-24 15:21:23 -0400

    Time: 143 ms (30.85%), Space: 78.7 MB (20.26%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,45 +1,47 @@
 class Node:
-    def __init__(self, key, val):
+    def __init__(self, key , val):
         self.key = key
         self.val = val
-        self.prev = self.next = None
+        self.prev = None
+        self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
+        self.cap = capacity
         self.cache = {}
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
-    
-    def insert(self, node):
-        prevnode , nex = self.right.prev , self.right
-        prevnode.next = nex.prev = node
-        node.prev, node.next = prevnode, nex
+        self.front = Node(0 , 0)
+        self.end = Node(0, 0)
+
+        self.front.nxt , self.end.prev = self.end , self.front
+
+    def addtoend(self, node):
+        last = self.end.prev
+        last.next , self.end.prev = node, node
+        node.prev, node.next = last, self.end
+
     def remove(self, node):
-        prev , nex = node.prev, node.next
-        prev.next, nex.prev = nex, prev
+        prevnode = node.prev
+        nextnode = node.next
+        prevnode.next , nextnode.prev = nextnode, prevnode
 
     def get(self, key: int) -> int:
         if key in self.cache:
             node = self.cache[key]
             self.remove(node)
-            self.insert(node)
+            self.addtoend(node)
             return node.val
         return -1
-        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-        if len(self.cache) > self.capacity:
-            node = self.left.next
-            self.remove(self.cache[node.key])
+        self.addtoend(self.cache[key])
+        if len(self.cache) > self.cap:
+            node = self.front.next
+            self.remove(node)
             del self.cache[node.key]
-        
-
 
 
 # Your LRUCache object will be instantiated and called as such:
