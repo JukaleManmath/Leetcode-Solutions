3de677e5266f4ff1a5cc73cb66ae0ed8133a4fa9
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-03-24 16:11:44 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,24 +1,25 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort(reverse = True)
         n = len(coins)
-        prev , curr = [0] * (amount + 1), [0] *(amount + 1)
+        dp = [[float("inf") for _ in range(amount + 1)] for _ in range(n)] 
+        def dfs(i, tar):
+            if i >= n:
+                if tar == 0:
+                    return 0
+                else:
+                    return float("inf")
+            if dp[i][tar] != float("inf"):
+                return dp[i][tar]
+            if tar == 0:
+                return 0
+            take = float("inf")
+            if coins[i] <= tar:
+                take = dfs(i + 1 , tar % coins[i]) + (tar // coins[i])
+            notake = dfs(i  + 1, tar)
 
-        for i in range(amount + 1):
-            if i % coins[0] == 0:
-                prev[i] = i // coins[0]
-            else:
-                prev[i] = float("inf")
-        
-        for i in range(1,n):
-            for j in range(amount + 1):
-                notake = prev[j]
-                take = float("inf")
-                if coins[i] <= j:
-                    take = 1 + curr[j - coins[i]]
-                curr[j] = min(take, notake)
-            prev = curr
-        
-        ans = prev[amount]
-        if ans == float("inf"):
-            return -1
-        return ans
+            dp[i][tar] =  min(take, notake)
+            return dp[i][tar]
+        res  = dfs(0 , amount)
+        return res if res != float("inf") else -1
+
