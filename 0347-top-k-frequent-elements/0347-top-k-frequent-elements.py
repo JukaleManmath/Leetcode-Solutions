aeeca8bf636acdfd90d26afc92f781d6bc8a77d6
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-03-24 13:29:29 -0400

    Time: 3 ms (88.58%), Space: 22.8 MB (89.53%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,31 +1,29 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # freq = Counter(nums)
-        # minHeap = []
-
-        # for i , val in freq.items():
-        #     heapq.heappush(minHeap, (-val, i))
-        
-        # res = []
-        # while k:
-        #     _, num = heapq.heappop(minHeap)
-        #     res.append(num)
-        #     k -= 1
-        # return res
-
-        n = len(nums)
-        buckets= [[] for _ in range(n + 1)] 
         freq = Counter(nums)
-        for i, val in freq.items():
-            buckets[val].append(i)
-        print(buckets)
-        res = []
-        i = n
-        while k and i >= 0:
-            if buckets[i]:
-                for num in buckets[i]:
-                    res.append(num)
-                    k -= 1
-            i -= 1
+        minHeap = []
 
-        return res
+        for i , val in freq.items():
+            heapq.heappush(minHeap, (val, i))
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+        return [ i for _, i in minHeap]
+        
+        
+
+        # n = len(nums)
+        # buckets= [[] for _ in range(n + 1)] 
+        # freq = Counter(nums)
+        # for i, val in freq.items():
+        #     buckets[val].append(i)
+        # print(buckets)
+        # res = []
+        # i = n
+        # while k and i >= 0:
+        #     if buckets[i]:
+        #         for num in buckets[i]:
+        #             res.append(num)
+        #             k -= 1
+        #     i -= 1
+
+        # return res
