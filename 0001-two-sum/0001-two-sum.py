60e40488eacf4a8131c31620cd1819228e9d7d7c
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-03-23 21:28:01 -0400

    Time: 0 ms (100%), Space: 20.7 MB (7.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,8 +3,7 @@ class Solution:
         mp = defaultdict(int)
 
         for i in range(len(nums)):
-
-            if target - nums[i] in mp:
-                return [mp[target - nums[i]] , i]
-            mp[nums[i]] = i
+            if nums[i] in mp:
+                return [mp[nums[i]], i]
+            mp[target - nums[i]] = i
         
