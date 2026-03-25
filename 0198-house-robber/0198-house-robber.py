dc36354959ffbc48ef2ce2d5a7db9dd8c8417115
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-03-25 01:54:28 -0400

    Time: 0 ms (100%), Space: 19.3 MB (78.11%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,12 +3,12 @@ class Solution:
         n = len(nums)
         dp = [-1 for _ in range(n)]
         def rec(i):
-            if i >= n:
+            if i < 0:
                 return 0
             if dp[i] != -1:
                 return dp[i]
-            take = nums[i] + rec (i + 2)
-            notake = rec(i + 1)
+            take = nums[i] + rec (i - 2)
+            notake = rec(i - 1)
             dp[i] = max(take, notake)
             return dp[i]
-        return rec(0)
+        return rec(n - 1)
