9a3f14ff753bfd868a3f4983332711bf113570ed
Manmath Jukale <90751985+JukaleManmath@users.noreply.github.com>
2026-03-25 01:50:30 -0400

    Time: 25 ms (33.76%), Space: 20.4 MB (42.61%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,9 +1,16 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        target = n - 1
-        for i in range(n - 2, -1, -1):
-            if i + nums[i] >= target:
-                target = i
+        # target = n - 1
+        # for i in range(n - 2, -1, -1):
+        #     if i + nums[i] >= target:
+        #         target = i
             
-        return target == 0
+        # return target == 0
+
+        canJump = 0
+        for i in range(n- 1):
+            if canJump < i:
+                return False
+            canJump = max(canJump ,i + nums[i])
+        return canJump >= n - 1
